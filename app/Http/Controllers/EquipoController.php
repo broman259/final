59fd51c953f6868234c4aca3c38626ef7134c339
Bryan Roman <bromang@miumg.edu.gt>
2024-10-26 00:19:17 -0600

    validando subida y carga de imagenes8 final, no se pudo

--- app/Http/Controllers/EquipoController.php
+++ app/Http/Controllers/EquipoController.php
@@ -46,8 +46,8 @@ class EquipoController extends Controller
         if($imagen = $request->file('imagen')){
             $rutaGuardarImg = 'imagen/';
             $imagenEquipo = date('YmdHis') . "." . $imagen->getClientOriginalExtension();
-            // $imagen->move(public_path($rutaGuardarImg), $imagenEquipo);
-            $imagen->storeAs($rutaGuardarImg, $imagenEquipo, 'public');
+            $imagen->move(public_path($rutaGuardarImg), $imagenEquipo);
+            // $imagen->storeAs($rutaGuardarImg, $imagenEquipo, 'public');
             $equipo['imagen'] = $imagenEquipo;
         }
             
